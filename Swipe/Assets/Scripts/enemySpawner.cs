b7ddbf0efb931231b2153aaf40eac5eba6d64269
Mohamed Dewidar <Mohamed Dewidar>
2016-12-03 16:42:04 +0000

    Added enemy spawner that spawns enemys however gravity doesn't apply properly, since they are also out of viewport they die instantly

---
+++ Swipe/Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySpawner : MonoBehaviour {
+
+    public float rate = 1;
+
+    private float timerStart = 0;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerStart += Time.deltaTime;
+        if (timerStart > rate)
+        {
+            // time is up...spawn a new enemy
+            var enemy = (GameObject)Instantiate(Resources.Load("Enemy"), transform.localPosition, Quaternion.identity, transform);
+            timerStart = 0;
+            enemy.transform.position += new Vector3(Random.Range(-500, 500), 0, 0 );
+        }
+	}
+}
